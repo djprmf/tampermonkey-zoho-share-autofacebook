ee47f03bc522122d58980602c5ead165edf53514
Pedro Fernandes <6960243+djprmf@users.noreply.github.com>
2020-07-08 10:05:42 +0100

    Add files via upload

---
+++ Zoho share para o Facebook.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Zoho Share to Facebook
+// @namespace    http://tugatech.com.pt/
+// @version      1.0
+// @description  Auto-select the Facebook option in the Zoho Share extension
+// @author       djprmf
+// @include      https://social.zoho.com/social/*
+// @grant        none
+// ==/UserScript==
+
+window.addEventListener('load', function() {
+    ReachSocialShare.PublishPost.switchEditorTab("fbPage_header",false);
+}, false);
+
